579ca9dbdd9251eafc60ca59a658561fcee922ac
ashish1500616 <ashish1500616@gmail.com>
2018-11-02 00:46:44 +0530

    V 1.0

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='visuals',
+    version='1.0',
+    description='A Visuls Code module',
+    author='Udacity',
+    author_email='example@example.com',
+    packages=['visuals'],  #same as name
+)
